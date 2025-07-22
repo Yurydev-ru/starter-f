c881d5f28aac70ed4c188ca53b376591d95bf52c
Yuryd <yurydevru@gmail.com>
2025-07-23 01:16:42 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,14 @@
 import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
-  ssr: false,
   nitro: {
     preset: 'vercel',
     compressPublicAssets: true,
     minify: true
   },
   css: ['~/assets/scss/main.scss'],
-  modules: ['@vite-pwa/nuxt'],
+  modules: [],
   vite: {
-    optimizeDeps: {
-    esbuildOptions: { target: 'es2022' }
-  },
       css: {
       preprocessorOptions: {
         scss: {
@@ -31,51 +27,51 @@ export default defineNuxtConfig({
     },
   },
   compatibilityDate: '2025-05-15',
-  pwa: {
-    manifest: {
-      name: 'Nuxt 3 Starter',
-      short_name: 'Nuxt 3 Starter',
-      description: 'Nuxt 3 Starter',
-      theme_color: '#ffffff',
-      lang: 'en',
-      icons: [
-        {
-          src: 'pwa-64x64.png',
-          sizes: '64x64',
-          type: 'image/png'
-        },
-        {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any'
-        },
-        {
-          src: 'maskable-icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'maskable'
-        }
-      ]
-    },
-    workbox: {
-      navigateFallback: '/',
-      globPatterns: ['**/*.{js,css,html,png,svg,ico}']
-    },
-    client: {
-      installPrompt: true,
-      periodicSyncForUpdates: 3600
-    },
-    devOptions: {
-      enabled: true,
-      suppressWarnings: true,
-      navigateFallbackAllowlist: [/^\/$/],
-      type: 'module'
-    }
-  }
+  // pwa: {
+  //   manifest: {
+  //     name: 'Nuxt 3 Starter',
+  //     short_name: 'Nuxt 3 Starter',
+  //     description: 'Nuxt 3 Starter',
+  //     theme_color: '#ffffff',
+  //     lang: 'en',
+  //     icons: [
+  //       {
+  //         src: 'pwa-64x64.png',
+  //         sizes: '64x64',
+  //         type: 'image/png'
+  //       },
+  //       {
+  //         src: 'pwa-192x192.png',
+  //         sizes: '192x192',
+  //         type: 'image/png'
+  //       },
+  //       {
+  //         src: 'pwa-512x512.png',
+  //         sizes: '512x512',
+  //         type: 'image/png',
+  //         purpose: 'any'
+  //       },
+  //       {
+  //         src: 'maskable-icon-512x512.png',
+  //         sizes: '512x512',
+  //         type: 'image/png',
+  //         purpose: 'maskable'
+  //       }
+  //     ]
+  //   },
+  //   workbox: {
+  //     navigateFallback: '/',
+  //     globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+  //   },
+  //   client: {
+  //     installPrompt: true,
+  //     periodicSyncForUpdates: 3600
+  //   },
+  //   devOptions: {
+  //     enabled: true,
+  //     suppressWarnings: true,
+  //     navigateFallbackAllowlist: [/^\/$/],
+  //     type: 'module'
+  //   }
+  // }
 });
